b791fc66bab1d51a2a427ab6d452e94a090848c5
tt527rutgers <119747699+tt527rutgers@users.noreply.github.com>
2022-12-03 10:59:50 -0500

    Update public class Main {.java

--- public class Main {.java
+++ public class Main {.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
-     
+        System.out.println("Guess number");
+        StdIn.readString();
+        System.out.println("Ok");
     }
 }
