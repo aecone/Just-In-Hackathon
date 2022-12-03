42f3ecad493f45e72d2ce85faa0d05ea7fddeece
tt527rutgers <119747699+tt527rutgers@users.noreply.github.com>
2022-12-03 10:45:25 -0500

    Update public class Main {.java

--- public class Main {.java
+++ public class Main {.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("How many dollars do you have?");
-        int money = StdIn.readInt();
-        System.out.println("You have $" & money & ". That is a lot.");
+     
     }
 }
