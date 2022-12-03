8c3e79896fe4961c8f7217c46c1fcd1915166b4c
tt527rutgers <119747699+tt527rutgers@users.noreply.github.com>
2022-12-03 10:33:30 -0500

    Update public class Main {.java

--- public class Main {.java
+++ public class Main {.java
@@ -1,3 +1,7 @@
 public class Main {
-    
+    public static void main(String[] args){
+        System.out.println("How many dollars do you have?");
+        int money = StdIn.readInt();
+        System.out.println("You have $" & money & ". That is a lot.");
+    }
 }
